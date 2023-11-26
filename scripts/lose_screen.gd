48cdfbbc65c58ba7de5000b9f4ec49556f626373
SamuelValentin974 <samuel.valentin@epitech.eu>
2023-11-26 11:31:35 +0400

    pushed some geysers and fixed menus

--- scripts/lose_screen.gd
+++ scripts/lose_screen.gd
@@ -10,7 +10,7 @@ func _on_quit_btn_pressed():
 	get_tree().quit()
 
 func _on_retry_btn_pressed():
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	get_tree().change_scene_to_file("res://scenes/MGame.tscn")
 	
 func SetScoreLabel(score):
 	_scoreLabel.text = "Score : " + str(score)
--- scripts/menu_screen.gd
+++ scripts/menu_screen.gd
@@ -5,4 +5,4 @@ func _process(delta):
 		get_tree().quit()
 
 func _on_play_btn_pressed():
-	get_tree().change_scene_to_file("res://scenes/level.tscn")
+	get_tree().change_scene_to_file("res://scenes/MGame.tscn")
