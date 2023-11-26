0db66d8f204caed0528c167111cbea592e0d6469
SamuelValentin974 <samuel.valentin@epitech.eu>
2023-11-26 11:46:59 +0400

    game finished

--- scripts/gas_btn.gd
+++ scripts/gas_btn.gd
@@ -19,25 +19,28 @@ var rng = RandomNumberGenerator.new()
 func _process(delta):
 	if _playerNear:
 		if Input.is_action_just_pressed("action") && _able:
+			_timerAble.start()
 			ResetTimer()
 			_clickSound.play( )
 			_able = false
 
 func _ready():
-	_timeBtn = rng.randi_range(12, 20)
+	_timeBtn = rng.randi_range(5, 20)
 	_timeLeft = _timeBtn
+	_cdGas = _cdAble
+
 	_timer = Timer.new()
 	_timer.name = "TimerBtn"
 	_timer.wait_time  = 1
 	_timer.timeout.connect(on_gas_btn_timer_timeout)
+	add_child(_timer)
+	
 	_timerAble = Timer.new()
 	_timerAble.name = "Timer cd"
 	_timerAble.wait_time = 1
 	_timerAble.timeout.connect(on_gas_cd_timer_timeout)
 	add_child(_timerAble)
-	add_child(_timer)
 	_timer.start()
-	_cdGas = _cdAble
 	SetTimeLabel(_timeLeft)
 
 func _on_body_entered(body):
@@ -45,7 +48,7 @@ func _on_body_entered(body):
 		_playerNear = true
 
 func ResetTimer():
-	_timeBtn = rng.randi_range(8, 15)	
+	_timeBtn = rng.randi_range(5, 20)	
 	_timeLeft = _timeBtn
 	SetTimeLabel(_timeLeft)
 	if !_actived :
@@ -75,3 +78,4 @@ func on_gas_cd_timer_timeout():
 		if (_cdGas < 0):
 			_able = true
 			_cdGas = _cdAble
+			_timerAble.stop()
