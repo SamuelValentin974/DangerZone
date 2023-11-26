0008cc06544b1a595d48084fd9b1bbe3beab89a0
SamuelValentin974 <samuel.valentin@epitech.eu>
2023-11-26 11:14:48 +0400

    gas btn wip

--- scripts/gas_btn.gd
+++ scripts/gas_btn.gd
@@ -1,30 +1,43 @@
 extends Area2D
 class_name GasBtn
 
-@export var _timeBtn = 5
+@export var _timeBtn = 0
 
 @onready var _timeLabel = $TimeLabel
 @onready var _clickSound = $ClickSound
 
 var _timeLeft = 0
 var _timer
+var _timerAble
 var _actived : bool = true
 var _playerNear = false
+var _able : bool = true
+var _cdAble = 10
+var _cdGas = 0
+var rng = RandomNumberGenerator.new()
 
 func _process(delta):
 	if _playerNear:
-		if Input.is_action_just_pressed("action"):
+		if Input.is_action_just_pressed("action") && _able:
 			ResetTimer()
 			_clickSound.play( )
+			_able = false
 
 func _ready():
+	_timeBtn = rng.randi_range(12, 20)
 	_timeLeft = _timeBtn
 	_timer = Timer.new()
 	_timer.name = "TimerBtn"
 	_timer.wait_time  = 1
 	_timer.timeout.connect(on_gas_btn_timer_timeout)
+	_timerAble = Timer.new()
+	_timerAble.name = "Timer cd"
+	_timerAble.wait_time = 1
+	_timerAble.timeout.connect(on_gas_cd_timer_timeout)
+	add_child(_timerAble)
 	add_child(_timer)
 	_timer.start()
+	_cdGas = _cdAble
 	SetTimeLabel(_timeLeft)
 
 func _on_body_entered(body):
@@ -32,6 +45,7 @@ func _on_body_entered(body):
 		_playerNear = true
 
 func ResetTimer():
+	_timeBtn = rng.randi_range(8, 15)	
 	_timeLeft = _timeBtn
 	SetTimeLabel(_timeLeft)
 	if !_actived :
@@ -54,3 +68,10 @@ func isActive():
 func _on_body_exited(body):
 	if body is Player:
 		_playerNear = false
+
+func on_gas_cd_timer_timeout():
+	if !_able :
+		_cdGas -= 1
+		if (_cdGas < 0):
+			_able = true
+			_cdGas = _cdAble
